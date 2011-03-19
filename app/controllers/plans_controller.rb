aab7a6395bd16b784f1e6d3816ff4254f8605d53
Ian Young <ian.greenleaf@gmail.com>
2011-03-18 23:50:35 -0700

    Resourceful routes

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,22 +1,22 @@
-class PlanController < ApplicationController
-  
+class PlansController < ApplicationController
+
   def edit
    @plan = current_account.plan
   end
-  
+
   def update
     @plan = current_account.plan
     @plan.edit_text = params[:plan][:edit_text]
     if @plan.save
-      redirect_to :action=>:read, :username => @plan.account.username
+      redirect_to :action=>:show, :id => @plan.account.username
     else
       render :action => "edit"
     end
   end
-  
-  def read
-      @plan = Account.find_by_username(params[:username]).plan
+
+  def show
+      @plan = Account.find_by_username(params[:id]).plan
       # TODO mark as read
   end
-  
+
 end
