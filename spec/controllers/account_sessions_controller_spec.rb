3266a04e5ee79c01820df5aa4995831d28b4518f
Ian Young <ian.greenleaf@gmail.com>
2011-03-20 00:45:45 -0700

    Replace account sessions controller tests with rspec

---
+++ spec/controllers/account_sessions_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe AccountSessionsController do
+  describe "new" do
+    subject { get :new }
+    it { response.should be_success }
+    it { should render_template :new }
+  end
+
+  context "with user" do
+    before do
+      Account.create!( :username => "foobar", :password => "foobar", :password_confirmation => "foobar" )
+    end
+
+    describe "login" do
+      before { post :create, :account_session => { :username => "foobar", :password => "foobar" } }
+      it { response.should be_redirect }
+    end
+
+    describe "failed login" do
+      before do
+        post :create, :account_session => { :username => "foobar", :password => "barbaz" }
+      end
+      it { response.should be_success }
+      it { should render_template :new }
+    end
+  end
+end
