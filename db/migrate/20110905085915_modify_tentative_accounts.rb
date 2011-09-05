de262d95e89f619a6fce2e8828f7177dfc246a58
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2011-09-05 05:03:31 -0700

    added partial registration code and modified the tentative account table

---
+++ db/migrate/20110905085915_modify_tentative_accounts.rb
@@ -0,0 +1,24 @@
+class ModifyTentativeAccounts < ActiveRecord::Migration
+  def self.up
+    drop_table :tentative_accounts
+    create_table :tentative_accounts do |t|
+      t.string :username, :email, :type, :confirmation_token
+      t.timestamps
+      t.primary_key :username
+    end
+    add_index :tentative_accounts, :username
+    add_index :tentative_accounts,:email
+    add_index :tentative_accounts,:confirmation_token
+  end
+
+  def self.down
+    drop_table :tentative_accounts
+    create_table :tentative_accounts do |t|
+      t.integer :tentative_accounts_id
+      t.string :token, :length => 16
+      t.string :session => 200
+      t.timestamps
+      t.primary_key :tentative_accounts_id
+    end
+  end
+end
