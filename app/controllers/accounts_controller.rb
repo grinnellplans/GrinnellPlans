14737d1c9169ad25fcde817213eb07962113a48e
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2011-09-13 01:55:43 -0700

    creating account on email confirmation link

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -11,6 +11,7 @@ class AccountsController < ApplicationController
   def create_random_token length=8
     characters = ('A'..'Z').to_a + (0..9).to_a
     characters -= ['B'] # B and 8 look very similar
+    characters -= ['O'] # O and 0 look very similar
     characters.sort_by{rand}
     (0..length).map{characters.sample}.join
   end
@@ -47,24 +48,30 @@ class AccountsController < ApplicationController
     send_confirmation_email ta.username, ta.email, ta.confirmation_token
     @confirmation_email_sent = true
   end
-
+  
   #TODO: replace all the booleans and hardcoded view logic with better notifications and messaging
   
   def confirm
     token = params[:token]
     ta = TentativeAccount.find_by_confirmation_token(token)
     
-    if !ta || ta.created_at > (Time.now - 1.day)
+    if !ta || ta.created_at < (Time.now - 1.day)
       redirect_to :action => 'new' 
     else
+      password = SecureRandom.hex(10)
+      email = Notifier.send_password ta.username, ta.email, password
       ActiveRecord::Base.transaction do
-        # create account
-
+        a = Account.create( :username => ta.username,
+                            :email => ta.email, 
+                            :user_type => ta.user_type,
+                            :password => password,
+                            :password_confirmation => password )
         ta.delete
       end
-    end     
+      email.deliver
+    end
   end
-
+  
   def resend_confirmation_email
     @username = params[:username]
     render :new if !@username
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -8,4 +8,10 @@ class Notifier < ActionMailer::Base
     mail :to => email, :subject => "Plan Activation Link"
   end
   
+  def send_password username, email, password
+    @username = username
+    @password = password
+    mail :to => email, :subject => "Plan Created"
+  end
+
 end
