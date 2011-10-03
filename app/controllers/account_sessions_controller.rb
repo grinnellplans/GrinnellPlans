d23c76d40d1f7acec103c3f2762b77b0ba7c74a2
Anna <acarey@gmail.com>
2011-10-02 23:37:27 -0400

    update db for latest login

--- app/controllers/account_sessions_controller.rb
+++ app/controllers/account_sessions_controller.rb
@@ -8,6 +8,9 @@ class AccountSessionsController < ApplicationController
     @session = AccountSession.new params[ :account_session ]
     if @session.save
       session[:autofinger_level] = 1
+      account = Account.where(:username => @session.username).first #todo - I bet there's a better way to do this line
+      account.login = Time.now
+      account.save!
       redirect_to "/"
     else
       render :action => :new
@@ -19,4 +22,5 @@ class AccountSessionsController < ApplicationController
     flash[ :notice ] = "You've been logged out."
     redirect_to new_account_session_path
   end
+  
 end
