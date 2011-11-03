9bf14c3bf10441a2285419d656220329499ad99f
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2011-11-02 23:48:00 -0700

    finished functional tests for accounts_controller

--- test/functional/accounts_controller_test.rb
+++ test/functional/accounts_controller_test.rb
@@ -5,6 +5,7 @@ class AccountsControllerTest < ActionController::TestCase
   test "should get registration page" do
     get :new
     assert_response :success
+    assert_select 'form', /My Grinnell email is/
   end
 
   test "should create tentative account and send email" do
@@ -78,18 +79,33 @@ class AccountsControllerTest < ActionController::TestCase
     get :confirm, { :token => 'PLAN9' }
     assert_select 'p', /Thank you for confirming your email/
 
+    # verify account
     account = Account.find_by_username 'plans'
     assert_not_nil account
     assert_equal ta.email, account.email
+    
+    # verify welcome email
+    email = ActionMailer::Base.deliveries.first
+    assert_equal 'Plan Created', email.subject
+    assert_equal 'plans@plans.plans', email.to[0]
+    assert_match 'Your Plan has been created!', email.body
+    assert_match 'password', email.body
   end
-
-  test "confirm welcome email" do
-  end
-
+  
   test "confirm with expired tentative account" do
+    ta = TentativeAccount.create( :username => 'plans',
+                                  :user_type => 'student',
+                                  :email => 'plans@plans.plans',
+                                  :confirmation_token => 'PLAN9',
+                                  :created_at => Time.now - 2.days,
+                                  :updated_at => Time.now - 2.days )
+    get :confirm, { :token => 'PLAN9' }
+    assert_redirected_to :controller => 'accounts', :action => 'new' 
   end
 
   test "confirm with wrong token" do
+    get :confirm, { :token => 'SHOO' }
+    assert_redirected_to :controller => 'accounts', :action => 'new' 
   end
   
   test "resend confirmation email" do
