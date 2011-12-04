b50d6d86ee2f2065eccc50fa843ee24f6ce28382
Ian Young <ian.greenleaf@gmail.com>
2011-12-04 15:24:25 -0800

    Pare down plan factory

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,8 +1,4 @@
 Factory.define :plan do |p|
-  p.add_attribute :plan,  8.times.collect{"blah "}.reduce(:+)
-  p.add_attribute :edit_text,  8.times.collect{"blah "}.reduce(:+)
-  def p.default_account
-    @default_account ||= Factory(:account)
-  end
-  p.user_id { p.default_account.userid}
+  p.edit_text 8.times.collect{"blah "}.reduce(:+)
+  p.association :account
 end
