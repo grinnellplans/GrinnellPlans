0602018e4a643d9ce62939ca692bf0453a6c9299
Ian Young <ian.greenleaf@gmail.com>
2012-03-05 23:22:34 -0800

    Refactor autofinger loading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   end
   
   def load_autofingers
-    @autofingers = Autofinger.where(:owner=>current_account.userid, :priority=> session[:autofinger_level], :updated => "1")
+    @autofingers = current_account.interests_in_others.where :priority=> session[:autofinger_level], :updated => "1"
   end
   
 end
