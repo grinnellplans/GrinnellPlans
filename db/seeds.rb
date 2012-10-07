d0894710bc9c5958244f8a246d48d15c72068a2a
Ubuntu <ubuntu@einstein.(none)>
2012-10-07 07:58:06 +0000

    adding absolute reference to ActiveRecord::Fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ require 'active_record/fixtures'
 
 puts "Loading #{Rails.env} seeds"
 Dir[Rails.root.join("db/seed", Rails.env, "*.{yml,csv}")].each do |file|
-  Fixtures.create_fixtures("db/seed/#{Rails.env}", File.basename(file, '.*'))
+  ActiveRecord::Fixtures.create_fixtures("db/seed/#{Rails.env}", File.basename(file, '.*'))
 end
