83c273bcb366cceb80d21e3ab8c2148a45627105
Shitanshu Aggarwal <saggarwal.90@gmail.com>
2012-10-08 07:35:11 +0000

    disabling layout on registration page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../../mailers/notifier.rb', __FILE__)
 
 class AccountsController < ApplicationController
+  layout false
 
   def new
     @allowed_domains = APP_CONFIG['email_domains'].map{|d| [d]}
