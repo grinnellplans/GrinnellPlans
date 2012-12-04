a44c5e2e999e988d71f3c61ffc2311b1af7bacb6
Ian Lunderskov <ilunderskov@gmail.com>
2012-12-03 22:53:37 -0600

    Validations for Style and Interface models.
    Fixes for [#37][https://github.com/annaswims/GrinnellPlans/issues/37] and [#38][https://github.com/annaswims/GrinnellPlans/issues/38].

--- spec/models/interface_spec.rb
+++ spec/models/interface_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe Interface do
+  it "must have a path" do
+    interface = described_class.create(:path => nil).should be_invalid
+  end
 end
-# == Schema Information
-#
-# Table name: interface
-#
-#  interface :integer         not null, primary key
-#  path      :string(128)
-#  descr     :string(255)
-#
-
