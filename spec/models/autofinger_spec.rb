8d18299bece9ac5d21d6b904466931218229d28f
Ian Young <ian.greenleaf@gmail.com>
2012-12-17 22:18:27 -0800

    Fix composite keys on wonky autofinger attrs

--- spec/models/autofinger_spec.rb
+++ spec/models/autofinger_spec.rb
@@ -4,7 +4,7 @@ describe Autofinger do
   it "must have a priority" do
     owner = Account.create! :username => "owner", :password => "123456", :password_confirmation => "123456"
     interest = Account.create! :username => "interest", :password => "123456", :password_confirmation => "123456"
-    autofinger = described_class.create(:owner => owner, :interest => interest, :priority => nil).should be_invalid
+    described_class.new(:owner => owner.id, :interest => interest.id, :priority => nil).should be_invalid
   end
 
   describe "updated scope" do
