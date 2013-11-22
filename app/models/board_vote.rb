44d6c90388fd54719bb5a56f7f38b7cfccc5e3d6
Anna <acarey@discoversdc.com>
2013-11-21 23:19:41 -0500

    Fixed typo in attribute names

--- app/models/board_vote.rb
+++ app/models/board_vote.rb
@@ -2,8 +2,8 @@ class BoardVote  < ActiveRecord::Base
   self.table_name = 'boardvotes'
   self.primary_key = :voteid
   belongs_to :account, foreign_key: :userid
-  belongs_to :main_board, foreign_key: :thredid
-  belongs_to :sub_board, foreign_key: :message_id
+  belongs_to :main_board, foreign_key: :threadid
+  belongs_to :sub_board, foreign_key: :messageid
   validates_presence_of :main_board, :sub_board, :account, :vote_date
 
   before_update :set_vote_date
