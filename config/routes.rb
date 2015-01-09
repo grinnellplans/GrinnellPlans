c1576fdfcc00ec367ee37c525de465447b159a02
Betsy Lorton <elorton@gmail.com>
2015-01-09 16:33:48 -0600

    Remove unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,5 @@
 Plans::Application.routes.draw do
 
-  # map.connect ':controller', :action => 'index'
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # See how all your routes lay out with "rake routes"
-
-  # You can have the root of your site routed with "root"
-
   # just remember to delete public/index.html.
   root controller: 'plans', action: 'show', id: 'plans'
 
