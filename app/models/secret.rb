6a6f5e862c619878e531d3bf19370fef40a7d6be
Anna <acarey@gmail.com>
2015-09-25 17:56:25 -0400

    Controller tests for secrets and Strong parameters

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -1,11 +1,11 @@
 class Secret < ActiveRecord::Base
   self.primary_key = :secret_id
-  self.per_page = 5
+  self.per_page = 100
 
   DISPLAY_OPTIONS = %w{yes no}
-  validates_presence_of :secret_text
-  validates_inclusion_of :display_attr, in:  DISPLAY_OPTIONS
-  validates_length_of :secret_text, maximum: 16_777_215
+  validates :secret_text, presence: true
+  validates :display_attr, inclusion: { in:  DISPLAY_OPTIONS }
+  validates :secret_text, length: { maximum: 16_777_215 }
 
   before_validation on: :create do
     self.display_attr = 'no' if display_attr.blank?
@@ -16,10 +16,12 @@ class Secret < ActiveRecord::Base
   end
 
   before_update do
+    #in old plans date_approved was populated when display='no' too
     self.date_approved = Time.now
   end
 
   # can't have "display" attribute because of Object.display ruby method
+  # the "display" method has to do with why string.to_s is displayed in the tests
   class << self
     def instance_method_already_implemented?(method_name)
       return true if method_name == 'display'
