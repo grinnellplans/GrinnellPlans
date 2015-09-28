dc08590c211622814147aede920aaea3d7091fac
Anna <acarey@gmail.com>
2015-09-27 23:02:06 -0400

    Test logout and add strong_params

--- spec/controllers/account_sessions_controller_spec.rb
+++ spec/controllers/account_sessions_controller_spec.rb
@@ -9,20 +9,32 @@ describe AccountSessionsController do
 
   context 'with user' do
     before do
-      Account.create!(username: 'foobar', password: 'foobar', password_confirmation: 'foobar')
+      @account =  Account.create!(username: 'foobar', password: 'foobar', password_confirmation: 'foobar')
     end
 
     describe 'login' do
       before { post :create, account_session: { username: 'foobar', password: 'foobar' } }
-      it { expect(response).to be_redirect }
+      it { expect(assigns[:session].record.id).to eq  @account.id}
+      it { expect(response).to redirect_to(root_path) }
     end
 
     describe 'failed login' do
       before do
         post :create, account_session: { username: 'foobar', password: 'barbaz' }
       end
+      it { expect(assigns[:current_account]).to be_nil }
       it { expect(response).to be_success }
       it { is_expected.to render_template :new }
+    end
+
+    describe 'logout' do
+      before do
+        post :create, account_session: { username: 'foobar', password: 'foobar' }
+        post :destroy
+      end
+      it { expect(response).to redirect_to(login_path) }
+      it { expect(assigns[:current_account]).to be_nil }
+      it { expect(assigns[:current_account_session]).to be_nil }
     end
   end
 end
