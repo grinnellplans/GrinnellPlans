2655e019edca9c9dfb33585c2ec5e41812d987ba
Hanna Liebl <hanna@radarfirst.com>
2015-10-12 18:57:26 -0700

    Validate that ViewSecret has an account

--- spec/models/viewed_secret_spec.rb
+++ spec/models/viewed_secret_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe ViewedSecret do
+  it 'must have an account' do
+    viewed_secret = described_class.create(:account => nil).should be_invalid
+  end
 end
 # == Schema Information
 #
