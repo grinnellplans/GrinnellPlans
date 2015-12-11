1c440129000277661ee582ba5267cae96d3aecd4
Ian Young <ian@iangreenleaf.com>
2015-12-10 22:43:50 -0800

    Add a Reject button for secrets

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,12 @@
 $(function() {
   $(".update-on-click").on("click", function(event) {
     $.ajax({
-      method: "put", dataType: "script", url: this.href, data: { secret: $(this).data() }
+      method: "put",
+      dataType: "script",
+      url: this.href,
+      data: { secret: $(this).data() },
+      success: (function() { $(this).addClass("selected") }).bind(this),
+      error: function(_, e) { alert(e) },
     });
     event.preventDefault();
   });
