5f588b818498c49102a8c430b05e233b5fda0477
Ian Young <ian@iangreenleaf.com>
2016-01-19 22:53:54 -0800

    Bail if no style set
    This shouldn't happen in real life, but to make it easier in tests, do
    this.

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -2,7 +2,7 @@ module AccountHelper
   def user_stylesheet_url(account)
     return nil if account.nil?
     return account.custom_stylesheet.stylesheet if account.custom_stylesheet.present?
-
+    return nil if account.style.nil?
     stylesheet_url(account.style.path.sub /^styles\//, '')
   end
 end
