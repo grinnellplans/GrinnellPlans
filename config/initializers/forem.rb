bc8e3cc22963c73be5025383366b3013dc69b28d
Ian Young <ian@iangreenleaf.com>
2016-01-29 19:51:27 -0800

    Use standard email reply-to

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,6 +1,6 @@
 Forem.user_class = "Account"
 Forem.sign_in_path = "/login"
-Forem.email_from_address = "please-change-me@example.com"
+Forem.email_from_address = APP_CONFIG['from_address']
 Forem.moderate_first_post = false
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
