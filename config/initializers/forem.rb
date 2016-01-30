fa18a584710bc2487b3c7c39c5899802f45b0e90
Ian Young <ian@iangreenleaf.com>
2016-01-29 19:48:33 -0800

    Don't moderate first post on Notes
    We're all friends here, right?

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -1,6 +1,7 @@
 Forem.user_class = "Account"
 Forem.sign_in_path = "/login"
 Forem.email_from_address = "please-change-me@example.com"
+Forem.moderate_first_post = false
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
 Forem.per_page = 20
