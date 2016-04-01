036d693cef8f764b9b4e2c8aedbea6b9db19e2d5
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-03-31 21:40:34 -0400

    corrected both relations to the Account model to use 'class_name'

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,4 +1,4 @@
 class Block < ActiveRecord::Base
-  belongs_to :account, foreign_key: :blocked_userid
-  belongs_to :account, foreign_key: :blocking_userid
+  belongs_to :blocked_user, class_name: "Account", foreign_key: :blocked_userid
+  belongs_to :blocking_user, class_name: "Account", foreign_key: :blocking_userid
 end
