22c3d6a285a3fc3723e64c71363346c69b01e278
Ian Young <ian@iangreenleaf.com>
2016-10-18 17:09:01 -0500

    Only run blocking migration in development
    See comment in code for details.

--- db/migrate/20160305220019_create_blocks.rb
+++ db/migrate/20160305220019_create_blocks.rb
@@ -1,12 +1,17 @@
 class CreateBlocks < ActiveRecord::Migration
   def change
-    create_table "blocks", id: false, force: :cascade do |t|
-      t.integer "blocked_user_id",  limit: 3, default: 0, null: false
-      t.integer "blocking_user_id", limit: 3, default: 0, null: false
-    end
+    # This migration happened concurrently in the PHP version, so we don't
+    # want it to run in production, but we need to catch Rails dev installs
+    # up to the current state.
+    if Rails.env.development?
+      create_table "blocks", id: false, force: :cascade do |t|
+        t.integer "blocked_user_id",  limit: 3, default: 0, null: false
+        t.integer "blocking_user_id", limit: 3, default: 0, null: false
+      end
 
-    add_index "blocks", ["blocked_user_id", "blocking_user_id"], name: "unique_idx", unique: true
-    add_index "blocks", ["blocked_user_id"], name: "lovee_idx"
-    add_index "blocks", ["blocking_user_id"], name: "lover_idx"
+      add_index "blocks", ["blocked_user_id", "blocking_user_id"], name: "unique_idx", unique: true
+      add_index "blocks", ["blocked_user_id"], name: "lovee_idx"
+      add_index "blocks", ["blocking_user_id"], name: "lover_idx"
+    end
   end
 end
