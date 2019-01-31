257570bf64e57e84b4bd6eefd8c666b19ebaefc1
walwoodr <walwoodr@gmail.com>
2019-01-31 08:17:51 -0600

    testing for css path

--- spec/models/style_spec.rb
+++ spec/models/style_spec.rb
@@ -4,6 +4,15 @@ describe Style do
   it 'must have a path' do
     expect(described_class.create(path: nil)).to be_invalid
   end
+
+  describe 'path must be' do
+    it 'valid if a css file' do 
+      expect(described_class.create(path: 'hat/moose/loon.css')).to be_valid
+    end
+    it 'invalid if not a css file' do
+      expect(described_class.create(path: 'hat/moose/loon.js')).to be_invalid
+    end
+  end
 end
 
 # == Schema Information
